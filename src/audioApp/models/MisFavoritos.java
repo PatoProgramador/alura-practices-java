d71e187f040a540fe8385cbaa1e2b33881e049bc
PatoProgramador <skpates18@gmail.com>
2024-04-17 16:18:43 -0500

    Add Fav class method to songs and podcast

---
+++ src/audioApp/models/MisFavoritos.java
@@ -0,0 +1,11 @@
+package audioApp.models;
+
+public class MisFavoritos {
+    public void adicionar(Audio audio){
+        if(audio.getClasificacion() >= 8) {
+            System.out.println(audio.getTitulo() + " es uno de los favoritos del momento");
+        } else {
+            System.out.println(audio.getTitulo() + " también es uno de los favoritos.");
+        }
+    }
+}
--- src/audioApp/principal/AppPrincipal.java
+++ src/audioApp/principal/AppPrincipal.java
@@ -1,6 +1,7 @@
 package audioApp.principal;
 
 import audioApp.models.Cancion;
+import audioApp.models.MisFavoritos;
 import audioApp.models.Podcast;
 
 public class AppPrincipal {
@@ -12,15 +13,26 @@ public class AppPrincipal {
         Podcast miPodcast = new Podcast();
         miPodcast.setPresentador("Gabriela Aguilar");
         miPodcast.setTitulo("Cafe.Tech");
-
+        // cancion
         for (int i = 0; i < 100; i++) {
             miCancion.meGusta();
         }
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < 2000; i++) {
             miCancion.reproduce();
+        }
+        // Podcast
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
         }
 
         System.out.println("Total de reproducciones: " + miCancion.getTotalReproducciones());
         System.out.println("Total de me gusta: " + miCancion.getTotalMeGusta());
+
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.adicionar(miCancion);
+        favoritos.adicionar(miPodcast);
     }
 }
